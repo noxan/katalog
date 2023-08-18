8ed475333ed9fef2d5068f8ada9c4bd76d5353d3
Richard <richard@stromer.org>
2023-08-18 14:06:39 +0200

    remove existing styles and use mantine instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Center, Container, Button, Input } from "@mantine/core";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -13,22 +12,8 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <Container>
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         className="row"
@@ -37,16 +22,19 @@ function App() {
           greet();
         }}
       >
-        <input
+        <Input
           id="greet-input"
           onChange={(e) => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
-        <button type="submit">Greet</button>
+
+        <Button type="submit">Greet</Button>
       </form>
 
-      <p>{greetMsg}</p>
-    </div>
+      <Center>
+        <p>{greetMsg}</p>
+      </Center>
+    </Container>
   );
 }
 
