0d0e756fa1db37bc781d98cd84163432f104e997
Richard <richard@stromer.org>
2023-08-18 16:09:55 +0200

    read meta-inf container xml file and find root opf file

--- src/epub.ts
+++ src/epub.ts
@@ -1,13 +1,28 @@
 import { FileEntry, readBinaryFile } from "@tauri-apps/api/fs";
-import { unzip as unzipcb, Unzipped } from "fflate/browser";
+import { unzip as unzipcb, Unzipped, strFromU8 } from "fflate/browser";
+import { XMLParser } from "fast-xml-parser";
 
 const unzip = (buffer: Uint8Array): Promise<Unzipped> =>
   new Promise((resolve, reject) =>
     unzipcb(buffer, (err, data) => (err ? reject(err) : resolve(data)))
   );
 
+const readXml = (xml: string) => {
+  const parser = new XMLParser({ ignoreAttributes: false });
+  return parser.parse(xml);
+};
+
+const findOpfFile = (zip: Unzipped) => {
+  const opfFilePath = "META-INF/container.xml";
+  const text = strFromU8(zip[opfFilePath]);
+  const xml = readXml(text);
+  return xml.container.rootfiles.rootfile["@_full-path"];
+};
+
 export const readEpub = async (entry: FileEntry) => {
   const buffer = await readBinaryFile(entry.path);
-  const unzipped = await unzip(buffer);
-  console.log(unzipped);
+  const zip = await unzip(buffer);
+
+  const opfFilePath = findOpfFile(zip);
+  console.log(opfFilePath);
 };
