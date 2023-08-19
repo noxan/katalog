690fce9f6398fb40b5ae4f97b1b7c1c99ec1fa2d
Richard <richard@stromer.org>
2023-08-19 13:17:53 +0200

    replace grid with simplegrid and define break points for num of cols

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import {
   Center,
   Container,
   Button,
-  Grid,
   Card,
   Text,
   Image,
   Group,
+  SimpleGrid,
 } from "@mantine/core";
 import { BookEntry, initialize, initializeBook } from "./utils";
 
@@ -46,54 +46,68 @@ function App() {
   };
 
   return (
-    <Center>
-      <Container>
-        <h1>Welcome to Tauri!</h1>
+    <Container fluid>
+      <Center>
+        <Container>
+          <h1>Welcome to Tauri!</h1>
 
-        <Group mb="md">
-          <Button
-            disabled={status.startsWith("loading")}
-            onClick={initializeKatalog}
+          <Group mb="md">
+            <Button
+              disabled={status.startsWith("loading")}
+              onClick={initializeKatalog}
+            >
+              Reload
+            </Button>
+            <Text>{status}</Text>
+          </Group>
+        </Container>
+      </Center>
+
+      <SimpleGrid
+        cols={5}
+        breakpoints={[
+          { maxWidth: "96rem", cols: 4, spacing: "md" },
+          { maxWidth: "64rem", cols: 3, spacing: "md" },
+          { maxWidth: "48rem", cols: 2, spacing: "sm" },
+          { maxWidth: "36rem", cols: 1, spacing: "sm" },
+        ]}
+      >
+        {entries.map((entry) => (
+          <Card
+            key={entry.name}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
           >
-            Reload
-          </Button>
-          <Text>{status}</Text>
-        </Group>
-
-        <Grid gutter="lg">
-          {entries.map((entry) => (
-            <Grid.Col key={entry.name} span={3}>
-              <Card shadow="sm" padding="lg" radius="md" withBorder>
-                <Card.Section>
-                  <Image
-                    height={200}
-                    src={entry?.coverImage}
-                    alt="Book cover image"
-                    withPlaceholder
-                    placeholder={
-                      <Text align="center" m="xs">
-                        {entry.name}
-                      </Text>
-                    }
-                  />
-                </Card.Section>
-                <Group position="apart" mt="md">
-                  <Text
-                    style={{
-                      overflow: "hidden",
-                      whiteSpace: "nowrap",
-                      textOverflow: "ellipsis",
-                    }}
-                  >
-                    {displayTitle(entry)}
+            <Card.Section>
+              <Image
+                height={200}
+                src={entry?.coverImage}
+                alt="Book cover image"
+                withPlaceholder
+                placeholder={
+                  <Text align="center" m="xs">
+                    {entry.name}
                   </Text>
-                </Group>
-              </Card>
-            </Grid.Col>
-          ))}
-        </Grid>
-      </Container>
-    </Center>
+                }
+              />
+            </Card.Section>
+            <Group position="apart" mt="md">
+              <Text
+                style={{
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {displayTitle(entry)}
+              </Text>
+            </Group>
+          </Card>
+        ))}
+      </SimpleGrid>
+    </Container>
   );
 }
 
