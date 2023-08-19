dd387149b72334e49b11e0dfdb77962f9ea9c946
Richard <richard@stromer.org>
2023-08-20 01:02:29 +0200

    setup color scheme provider for mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { MantineProvider } from "@mantine/core";
+import { useState } from "react";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 import Katalog from "./Katalog";
 
 function App() {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider
-      theme={{ colorScheme: "dark" }}
-      withGlobalStyles
-      withNormalizeCSS
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Katalog />
-    </MantineProvider>
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Katalog />
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
