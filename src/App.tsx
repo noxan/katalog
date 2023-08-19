055e1ffd22cfae67656995c0221acab19402d5d7
Richard <richard@stromer.org>
2023-08-20 01:11:09 +0200

    remove logging from dark mode detection

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,13 @@ import Katalog from "./Katalog";
 
 function App() {
   const isMediaDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  console.log(isMediaDarkMode);
   const preferredColorScheme = useColorScheme();
-  console.log(preferredColorScheme);
   const [colorScheme, setColorScheme] =
     useState<ColorScheme>(preferredColorScheme);
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-  console.log("colorScheme", colorScheme);
   useEffect(() => {
-    console.log("useEffect", isMediaDarkMode);
     if (isMediaDarkMode) {
       setColorScheme("dark");
     } else {
