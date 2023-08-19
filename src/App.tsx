28b1e45ff54bc3a1d7ae7d7d97d7f25859beddc0
Richard <richard@stromer.org>
2023-08-20 01:09:33 +0200

    automatically detect dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   ColorScheme,
   ColorSchemeProvider,
   MantineProvider,
 } from "@mantine/core";
-import { useColorScheme } from "@mantine/hooks";
+import { useColorScheme, useMediaQuery } from "@mantine/hooks";
 import Katalog from "./Katalog";
 
 function App() {
+  const isMediaDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  console.log(isMediaDarkMode);
   const preferredColorScheme = useColorScheme();
+  console.log(preferredColorScheme);
   const [colorScheme, setColorScheme] =
     useState<ColorScheme>(preferredColorScheme);
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  console.log("colorScheme", colorScheme);
+  useEffect(() => {
+    console.log("useEffect", isMediaDarkMode);
+    if (isMediaDarkMode) {
+      setColorScheme("dark");
+    } else {
+      setColorScheme("light");
+    }
+  }, [preferredColorScheme]);
 
   return (
     <ColorSchemeProvider
