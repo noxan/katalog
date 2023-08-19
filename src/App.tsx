02d1a9934baa5f1b4dc64d5cfe6656ba7200db2b
Richard <richard@stromer.org>
2023-08-20 01:18:25 +0200

    extract theme setup to dedicated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from "@mantine/core";
-import { useColorScheme, useMediaQuery } from "@mantine/hooks";
 import Katalog from "./Katalog";
+import ThemeProvider from "./ThemeProvider";
 
 function App() {
-  const isMediaDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const preferredColorScheme = useColorScheme();
-  const [colorScheme, setColorScheme] =
-    useState<ColorScheme>(preferredColorScheme);
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
-  useEffect(() => {
-    if (isMediaDarkMode) {
-      setColorScheme("dark");
-    } else {
-      setColorScheme("light");
-    }
-  }, [preferredColorScheme]);
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{ colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        <Katalog />
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <ThemeProvider>
+      <Katalog />
+    </ThemeProvider>
   );
 }
 
