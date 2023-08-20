40cab9fe3450f2e82c3086099c28bebe0b33ce97
Richard <richard@stromer.org>
2023-08-20 21:29:32 +0200

    remove title from card and add scale hover effect

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,4 +1,4 @@
-import { AspectRatio, Card, Group, Image, Text } from "@mantine/core";
+import { AspectRatio, Card, Image, Text, createStyles } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { BookEntry } from "../helpers/utils";
 
@@ -7,40 +7,45 @@ const displayTitle = (entry: BookEntry) => {
   return typeof title === "object" ? title["#text"] : title;
 };
 
-export const BookCard = ({ entry }: { entry: BookEntry }) => (
-  <Card
-    key={entry.name}
-    shadow="sm"
-    padding="lg"
-    radius="md"
-    withBorder
-    component={Link}
-    to={`/books/${entry.name}`}
-  >
-    <Card.Section>
-      <AspectRatio ratio={0.75}>
-        <Image
-          src={entry?.coverImage}
-          alt="Book cover image"
-          withPlaceholder
-          placeholder={
-            <Text align="center" m="xs">
-              {entry.name}
-            </Text>
-          }
-        />
-      </AspectRatio>
-    </Card.Section>
-    <Group position="apart" mt="md">
-      <Text
-        style={{
-          overflow: "hidden",
-          whiteSpace: "nowrap",
-          textOverflow: "ellipsis",
-        }}
-      >
-        {displayTitle(entry)}
-      </Text>
-    </Group>
-  </Card>
-);
+const useStyles = createStyles((theme) => ({
+  card: {
+    position: "relative",
+    transition: "transform 100ms ease-in",
+    "&:hover": {
+      boxShadow: theme.shadows.md,
+      transform: "scale(1.02)",
+    },
+  },
+}));
+
+export const BookCard = ({ entry }: { entry: BookEntry }) => {
+  const { classes } = useStyles();
+
+  return (
+    <Card
+      key={entry.name}
+      shadow="sm"
+      padding="lg"
+      radius="md"
+      withBorder
+      component={Link}
+      to={`/books/${entry.name}`}
+      className={classes.card}
+    >
+      <Card.Section>
+        <AspectRatio ratio={0.75}>
+          <Image
+            src={entry?.coverImage}
+            alt="Book cover image"
+            withPlaceholder
+            placeholder={
+              <Text align="center" m="xs">
+                {entry.name}
+              </Text>
+            }
+          />
+        </AspectRatio>
+      </Card.Section>
+    </Card>
+  );
+};
