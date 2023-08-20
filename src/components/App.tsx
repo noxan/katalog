8de9ff47156de4bc7e558a69ef478e2503316291
Richard <richard@stromer.org>
2023-08-20 14:19:03 +0200

    render book detail page and select the related entry

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <KatalogRoute />,
   },
   {
-    path: "/books/:id",
+    path: "/books/:name",
     element: <BookRoute />,
   },
 ]);
--- src/routes/BookRoute.tsx
+++ src/routes/BookRoute.tsx
@@ -1,9 +1,17 @@
 import { Container, Text } from "@mantine/core";
+import { useContext } from "react";
+import { useParams } from "react-router";
+import { KatalogContext } from "../providers/KatalogProvider";
 
 export default function BookRoute() {
+  const { name } = useParams();
+  const { entries } = useContext(KatalogContext);
+  const entry = entries.filter((value) => value.name === name);
+
   return (
     <Container>
       <Text>BookRoute</Text>
+      <Text>{JSON.stringify(entry)}</Text>
     </Container>
   );
 }
