6c3bc9e37a028dbe3ff1cb585ad1c6cde24656a3
Richard <richard@stromer.org>
2023-08-20 14:21:06 +0200

    render cover image on book detail page

--- src/routes/BookRoute.tsx
+++ src/routes/BookRoute.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from "@mantine/core";
+import { Container, Image, Text } from "@mantine/core";
 import { useContext } from "react";
 import { useParams } from "react-router";
 import { KatalogContext } from "../providers/KatalogProvider";
@@ -6,11 +6,12 @@ import { KatalogContext } from "../providers/KatalogProvider";
 export default function BookRoute() {
   const { name } = useParams();
   const { entries } = useContext(KatalogContext);
-  const entry = entries.filter((value) => value.name === name);
+  const entry = entries.filter((value) => value.name === name)[0];
 
   return (
     <Container>
       <Text>BookRoute</Text>
+      <Image src={entry.coverImage} height={300} width={200} />
       <Text>{JSON.stringify(entry)}</Text>
     </Container>
   );
