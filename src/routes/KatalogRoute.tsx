23ff9e5c05828ec4469da5cd72bbebb0aa2a0b44
Richard <richard@stromer.org>
2023-08-20 13:43:24 +0200

    render proper katalog instead of placeholder

--- src/routes/KatalogRoute.tsx
+++ src/routes/KatalogRoute.tsx
@@ -1,18 +1,11 @@
 import { Container, Text } from "@mantine/core";
-import { useContext } from "react";
-import { KatalogContext } from "../KatalogProvider";
+import Katalog from "../Katalog";
 
 export default function KatalogRoute() {
-  const { entries } = useContext(KatalogContext);
-
   return (
     <Container>
       <Text>KatalogRoute</Text>
-      <ul>
-        {entries.map((entry) => (
-          <li key={entry.name}>{JSON.stringify(entry)}</li>
-        ))}
-      </ul>
+      <Katalog />
     </Container>
   );
 }
