6e2e233998dcaca1247d8ca2b77ac1226e47ba48
Richard <richard@stromer.org>
2023-08-20 13:14:03 +0200

    create placeholder routes for katalog and book view

---
+++ src/routes/BookRoute.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from "@mantine/core";
+
+export default function BookRoute() {
+  return (
+    <Container>
+      <Text>BookRoute</Text>
+    </Container>
+  );
+}
---
+++ src/routes/KatalogRoute.tsx
@@ -0,0 +1,9 @@
+import { Container, Text } from "@mantine/core";
+
+export default function KatalogRoute() {
+  return (
+    <Container>
+      <Text>KatalogRoute</Text>
+    </Container>
+  );
+}
