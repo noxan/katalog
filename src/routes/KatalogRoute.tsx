4ebb0952de671529c7df13400f77ab1875bcca6b
Richard <richard@stromer.org>
2023-08-20 13:16:33 +0200

    render list of entries from context

--- src/routes/KatalogRoute.tsx
+++ src/routes/KatalogRoute.tsx
@@ -1,9 +1,18 @@
 import { Container, Text } from "@mantine/core";
+import { useContext } from "react";
+import { KatalogContext } from "../KatalogProvider";
 
 export default function KatalogRoute() {
+  const entries = useContext(KatalogContext);
+
   return (
     <Container>
       <Text>KatalogRoute</Text>
+      <ul>
+        {entries.map((entry) => (
+          <li>{JSON.stringify(entry)}</li>
+        ))}
+      </ul>
     </Container>
   );
 }
