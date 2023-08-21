44da9dd3cf9d26cf4ad2fff51fae8ad95d72bebc
Richard <richard@stromer.org>
2023-08-21 11:36:17 +0200

    copy dropzone example from mantine

--- src/components/EmptyLibrary.tsx
+++ src/components/EmptyLibrary.tsx
@@ -1,9 +1,56 @@
-import { Button, Container, Text } from "@mantine/core";
-import { IconBooks } from "@tabler/icons-react";
+import {
+  Button,
+  Container,
+  Group,
+  Text,
+  rem,
+  useMantineTheme,
+} from "@mantine/core";
+import { Dropzone } from "@mantine/dropzone";
+import { IconBooks, IconPhoto, IconUpload, IconX } from "@tabler/icons-react";
 
 export function EmptyLibrary() {
+  const theme = useMantineTheme();
   return (
     <Container fluid mb="md">
+      <Dropzone>
+        <Group
+          position="center"
+          spacing="xl"
+          style={{ minHeight: rem(220), pointerEvents: "none" }}
+        >
+          <Dropzone.Accept>
+            <IconUpload
+              size="3.2rem"
+              stroke={1.5}
+              color={
+                theme.colors[theme.primaryColor][
+                  theme.colorScheme === "dark" ? 4 : 6
+                ]
+              }
+            />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <IconX
+              size="3.2rem"
+              stroke={1.5}
+              color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+            />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <IconPhoto size="3.2rem" stroke={1.5} />
+          </Dropzone.Idle>
+
+          <div>
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
       <IconBooks strokeWidth={1} size={128} />
 
       <Text>There are no books in the katalog.</Text>
