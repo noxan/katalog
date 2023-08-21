870c2a596ef01ed84a8c37a1c3ca96db01785fcf
Richard <richard@stromer.org>
2023-08-21 11:33:01 +0200

    add icon and button to empty library component

--- src/components/EmptyLibrary.tsx
+++ src/components/EmptyLibrary.tsx
@@ -1,9 +1,13 @@
-import { Container, Text } from "@mantine/core";
+import { Button, Container, Text } from "@mantine/core";
+import { IconBooks } from "@tabler/icons-react";
 
 export function EmptyLibrary() {
   return (
     <Container fluid mb="md">
+      <IconBooks strokeWidth={1} size={128} />
+
       <Text>There are no books in the katalog.</Text>
+      <Button mt="md">Import book</Button>
     </Container>
   );
 }
