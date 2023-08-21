1050b65ab8b4d7043e95d71ee0b153c914ab2820
Richard <richard@stromer.org>
2023-08-21 12:27:11 +0200

    extract replace entry by path to helper function

--- src/stores/katalog.ts
+++ src/stores/katalog.ts
@@ -9,6 +9,9 @@ interface KatalogStore {
   importBook: () => void;
 }
 
+const replaceEntryByPath = (entries: BookEntry[], newEntry: BookEntry) =>
+  entries.map((entry) => (entry.path === newEntry.path ? newEntry : entry));
+
 export const useKatalogStore = create<KatalogStore>((set) => ({
   status: KatalogStatus.INITIALIZE,
   entries: [],
@@ -23,11 +26,7 @@ export const useKatalogStore = create<KatalogStore>((set) => ({
     await Promise.all(
       entries.map(async (entry) => {
         const epub = await readEpub(entry);
-        set((state) => ({
-          entries: state.entries.map((entry) =>
-            entry.path === epub.path ? epub : entry
-          ),
-        }));
+        set((state) => ({ entries: replaceEntryByPath(state.entries, epub) }));
       })
     );
     set({ status: KatalogStatus.READY });
