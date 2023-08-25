53b304541d1399812cf1f8205d6ed5107a636700
Richard <richard@stromer.org>
2023-08-25 16:12:03 +0200

    define custom error types and return the node directly (instead of wrapped option)

--- src-tauri/src/xml.rs
+++ src-tauri/src/xml.rs
@@ -7,20 +7,27 @@ pub struct XMLNode {
     name: String,
 }
 
+#[derive(Debug, Clone)]
+pub enum XMLError {
+    FromUtf8Error(FromUtf8Error),
+    EmptyRoot,
+}
+
 pub struct XMLReader {}
 
 impl XMLReader {
-    pub fn parse(content: &[u8]) -> Result<Option<XMLNode>, FromUtf8Error> {
+    pub fn parse(content: &[u8]) -> Result<XMLNode, XMLError> {
         let mut reader = Reader::from_bytes(content);
         let mut buf = Vec::new();
 
         let mut root: Option<XMLNode> = None;
 
-        Ok(loop {
+        loop {
             match reader.read_event(&mut buf) {
                 Ok(Event::Start(ref e)) => {
                     let node = XMLNode {
-                        name: String::from_utf8(e.name().to_vec())?,
+                        name: String::from_utf8(e.name().to_vec())
+                            .map_err(XMLError::FromUtf8Error)?,
                     };
                     if root.is_none() {
                         root = Some(node);
@@ -52,11 +59,16 @@ impl XMLReader {
                 Ok(Event::Text(e)) => {
                     println!("Text: {:?}", e.unescape_and_decode(&reader).unwrap());
                 }
-                Ok(Event::Eof) => break root,
+                Ok(Event::Eof) => break,
                 _ => (),
             }
             buf.clear();
-        })
+        }
+
+        match root {
+            None => return Err(XMLError::EmptyRoot),
+            Some(root) => Ok(root),
+        }
     }
 }
 
@@ -68,7 +80,7 @@ mod tests {
     fn read_xml_test() {
         let xml =
             r#"<tag1 att1 = "test"><tag2><!--Test comment-->Test</tag2><tag2>Test 2</tag2></tag1>"#;
-        let xml = XMLReader::parse(xml.as_bytes()).unwrap().unwrap();
+        let xml = XMLReader::parse(xml.as_bytes()).unwrap();
         println!("XML: {:?}", xml);
 
         assert_eq!(xml.name, "tag1");
