668fbbbcc859909a92ec1d248e21d6b8cc60d267
Richard <richard@stromer.org>
2023-08-28 22:29:58 +0200

    open up zip file and merge error handling

--- src-tauri/src/book.rs
+++ src-tauri/src/book.rs
@@ -1,17 +1,21 @@
 use std::fs::File;
 
 use thiserror::Error;
-use zip::ZipArchive;
+use zip::{result::ZipError, ZipArchive};
 
 #[derive(Error, Debug)]
 pub enum BookError {
     #[error("I/O Error: {0}")]
     IOError(#[from] std::io::Error),
+    #[error("Zip Error: {0}")]
+    ZipError(#[from] ZipError),
 }
 
 fn edit(filepath: &str) -> Result<(), BookError> {
     let reader = File::open(filepath)?;
-    let archive = ZipArchive::new(reader);
+    let mut archive = ZipArchive::new(reader)?;
+
+    let root_file = archive.by_name("META-INF/container.xml")?;
 
     Ok(())
 }
