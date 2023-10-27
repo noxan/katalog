a9c04105e41404798c6e666bc3b7774107440758
Richard <richard@stromer.org>
2023-10-27 11:27:24 +0200

    use auto color scheme instead of a lot of custom code

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,39 +1,7 @@
-import { useEffect, useState } from "react";
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from "@mantine/core";
-import { useColorScheme, useMediaQuery } from "@mantine/hooks";
+import { MantineProvider } from "@mantine/core";
 
 export default function ThemeProvider({ children }: any) {
-  const isMediaDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const preferredColorScheme = useColorScheme();
-  const [colorScheme, setColorScheme] =
-    useState<ColorScheme>(preferredColorScheme);
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-
-  useEffect(() => {
-    if (isMediaDarkMode) {
-      setColorScheme("dark");
-    } else {
-      setColorScheme("light");
-    }
-  }, [preferredColorScheme]);
-
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{ colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
-      >
-        {children}
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
   );
 }
