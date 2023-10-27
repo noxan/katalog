7043b497ee2c0fbbb8f4948a1769bfb18a872ebf
Richard <richard@stromer.org>
2023-10-27 11:25:35 +0200

    add header height again and properly use app shell (not only header)

--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -4,8 +4,11 @@ import { KatalogHeader } from "../components/KatalogHeader";
 
 export default function RootRoute() {
   return (
-    <AppShell padding="md" header={<KatalogHeader />}>
-      <Outlet />
+    <AppShell padding="md" header={{ height: 56 }}>
+      <KatalogHeader />
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
     </AppShell>
   );
 }
