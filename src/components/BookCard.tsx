3798852254493ff0be50f0cf9affa70518a3aa77
Richard <richard@stromer.org>
2023-10-27 11:24:28 +0200

    migrate book card createstyles to css module

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,27 +1,13 @@
-import { AspectRatio, Card, Image, Text, createStyles } from "@mantine/core";
+import { AspectRatio, Card, Image } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { BookEntry } from "../types";
+import classes from "./BookCard.module.css";
 
 const displayTitle = (entry: BookEntry) => {
   return entry?.metadata?.title ?? entry.name;
 };
 
-const animationStyle = "150ms cubic-bezier(0.4,0,0.2,1)";
-
-const useStyles = createStyles((theme) => ({
-  card: {
-    position: "relative",
-    transition: `transform ${animationStyle}`,
-    "&:hover": {
-      boxShadow: theme.shadows.md,
-      transform: "scale(1.02)",
-    },
-  },
-}));
-
 export const BookCard = ({ entry }: { entry: BookEntry }) => {
-  const { classes } = useStyles();
-
   return (
     <Card
       key={entry.name}
