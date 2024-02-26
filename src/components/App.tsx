8e9d86c08af33557dc3daf6fdfeb915357955c92
Richard <richard@stromer.org>
2024-02-26 11:03:51 +0100

    fix undefined useeffect dependency

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -39,7 +39,7 @@ function App() {
       firstRun = false;
       initializeKatalog();
     }
-  }, []);
+  }, [initializeKatalog]);
   return (
     <MantineProvider defaultColorScheme="auto">
       <RouterProvider router={router} />
