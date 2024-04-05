deaef69ef8730d3ecdf0dde146ebea2ec66359e0
Richard <richard@stromer.org>
2024-04-04 20:32:42 -0400

    add foll screen drop zone to import new books

--- src/routes/KatalogRoute.tsx
+++ src/routes/KatalogRoute.tsx
@@ -2,9 +2,14 @@ import { Container, SimpleGrid } from "@mantine/core";
 import { BookCard } from "../components/BookCard";
 import { EmptyLibrary } from "../components/EmptyLibrary";
 import { useKatalogStore } from "../stores/katalog";
+import { Dropzone } from "@mantine/dropzone";
+import { ACCEPTED_MIME_TYPES } from "../types";
 
 export default function KatalogRoute() {
   const entries = useKatalogStore((state) => state.entries);
+  const copyBooksToKatalog = useKatalogStore(
+    (state) => state.copyBooksToKatalog,
+  );
 
   if (entries.length <= 0) {
     return <EmptyLibrary />;
@@ -12,6 +17,10 @@ export default function KatalogRoute() {
 
   return (
     <Container fluid mb="md">
+      <Dropzone.FullScreen
+        onDrop={copyBooksToKatalog}
+        accept={ACCEPTED_MIME_TYPES}
+      />
       <SimpleGrid
         cols={{ base: 1, xs: 2, sm: 3, md: 3, lg: 4, xl: 5 }}
         spacing={{ base: "sm", md: "md" }}
